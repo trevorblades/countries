d7bcd9941070dc38827b3fe5ece2def3d3a78b0e
Trevor Blades <tdblades@gmail.com>
2020-02-26 20:08:15 -0800

    Specify a port option

--- index.js
+++ index.js
@@ -136,6 +136,6 @@ const server = new ApolloServer({
   }
 });
 
-server.listen().then(({url}) => {
+server.listen({port: process.env.PORT}).then(({url}) => {
   console.log(`🚀  Server ready at ${url}`);
 });
